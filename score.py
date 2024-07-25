8bbdbdf65c6e7c0d66ccd981ba5303892607c7b7
arthurbenemann <arthur.benemann@gmail.com>
2024-07-24 19:19:45 -0700

    sort the matchmaking to avoid duplicates in dictionary

--- score.py
+++ score.py
@@ -54,12 +54,13 @@ def calcRatings(df: pd.DataFrame) -> pd.DataFrame:
             df.loc[index, player.name] = player.ordinal(alpha=200/model.sigma, target=1500) # Elo scaling
     return df, players, model
 
-def qualityMatches(df,players,model):
+
+def qualityMatches(df, players, model):
     df_last = df.iloc[[-1]]
     df_last = df_last.drop(columns=['date','win','loss'])
     two_player_combinations = list(combinations(players, 2))
-    
-        # Filter combinations to ensure the teams are disjoint
+
+    # Filter combinations to ensure the teams are disjoint
     matches = []
     for combo1 in two_player_combinations:
         remaining_players = set(players) - set(combo1)
@@ -67,14 +68,15 @@ def qualityMatches(df,players,model):
             if set(combo2).isdisjoint(set(combo1)):
                 # Sort and add the match to the list
                 match = [sorted(combo1), sorted(combo2)]
+                match.sort()
                 if match not in matches:
                     matches.append(match)
-    
+
     match_score ={}
     for match in matches:
         match_name = match[0][0].name + match[0][1].name+' vs '+ match[1][0].name+match[1][1].name
         match_score[match_name] = model.predict_draw(match)
-    
+
     df_matchscore = pd.DataFrame(match_score.items(),columns=['match', 'quality'])
 
     return df_matchscore
